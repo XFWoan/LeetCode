367f76cbaeafbe4599561353a2841df3c89897b2
xfwoan <xfwoan@163.com>
2020-09-04 15:56:41 +0800

    48

--- Solution/Solutions.cs
+++ Solution/Solutions.cs
@@ -620,5 +620,22 @@ namespace Solutions
                 }
             }
         }
+
+        //48. 旋转图像
+        static public void Rotate(int[][] matrix)
+        {
+            int n = matrix.Length;
+            for (int i = 0; i < (n+1)/2; i++)
+            {
+                for (int j = 0; j < n/2; j++)
+                {
+                    int temp = matrix[n - 1 - j][i];
+                    matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                    matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+                    matrix[j][n - 1 - i] = matrix[i][j];
+                    matrix[i][j] = temp;
+                }
+            }
+        }
     }
 }
